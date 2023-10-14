937763f4f402d4ac0163e86e23056657b36fde0f
carlos-awa <antoniteneria9315@gmail.com>
2023-10-14 14:50:21 -0600

    These are the first Metaphorce Challenge Archives

---
+++ src/test/java/com/example/challenge_crud/ChallengeCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.challenge_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
