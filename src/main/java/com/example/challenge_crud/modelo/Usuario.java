4b9f170793acd5dc67afdaad987c5086d65d37d0
carlos-awa <antoniteneria9315@gmail.com>
2023-10-14 23:27:30 -0600

    Se añadio Lombok y creo el primer modelo llamado "Usuario"

---
+++ src/main/java/com/example/challenge_crud/modelo/Usuario.java
@@ -0,0 +1,19 @@
+package com.example.challenge_crud.modelo;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table
+
+@Data
+@Getter
+@Setter
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id_usuario;
+    private String nombre;
+}
