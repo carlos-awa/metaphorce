de658becb0a599e9702fdd3a4d30456deb207121
CHOCL02 <20031563@itcelaya.edu.mx>
2023-12-12 00:44:14 -0600

    Loggs

--- src/main/java/com/example/supercitox/controlador/ControladorUsuario.java
+++ src/main/java/com/example/supercitox/controlador/ControladorUsuario.java
@@ -2,6 +2,8 @@ package com.example.supercitox.controlador;
 
 import com.example.supercitox.modelo.Usuario;
 import com.example.supercitox.servicio.ServicioUsuario;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "usuarios")
 public class ControladorUsuario {
+    private static final Logger logg = LoggerFactory.getLogger(ControladorUsuario.class);
     private final ServicioUsuario servicioUsuario;
     @Autowired
     public ControladorUsuario(ServicioUsuario servicioUsuario) {
@@ -19,11 +22,13 @@ public class ControladorUsuario {
 
     @GetMapping(path = "/mostrarUsuarios")
     public List<Usuario> mostrarUsuarios(){
+        logg.info("Aquí te va todo");
         return this.servicioUsuario.mostrarUsuarios();
     }
 
     @PostMapping(path = "/agregarUsuario")
     public ResponseEntity<Object> agregarUsuario(@RequestBody Usuario usuario){
+        logg.info("Información de nuevo usuario: {}", usuario);
         return this.servicioUsuario.agregarUsuario(usuario);
     }
 
